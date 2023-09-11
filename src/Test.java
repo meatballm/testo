90f23d8267081eed3ea7fb4e33be89594e814b5d
7312-015 <wh9555115@naver.com>
2023-09-11 17:42:40 +0900

    first commit

--- src/Test.java
+++ src/Test.java
@@ -4,14 +4,22 @@ public class Test extends Thread{
     public Test(int[] a){
         arr=a;
     }
+    public void run(){
+        int sum=0;
+        try {
+            for (int i = 0; i < 50; i++) {
+                sum += arr[i];
+            }
+        }catch(Exception e) {
+        }
+    }
     public static void main(String[] args){
 
         int[] array=new int[100];
         for(int i=0;i<100;i++){
             array[i]=i;
         }
-        int[] t1=Arrays.copyOfRange(array,0,49);
-        int[] t3=Arrays.copyOfRange(array,50,99);
-        Test th1=t1;
+        Test th1=new Test(Arrays.copyOfRange(array,0,49));
+        Test th2=new Test(Arrays.copyOfRange(array,50,99));
     }
 }
